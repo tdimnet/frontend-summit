14d9004f5c6d1c21d750b9d63bbda0b08901aa7a
tdimnet <thomas.dimnet@datadoghq.com>
2022-03-16 15:40:50 +0100

    Clean code and add hello, world widget

--- datadog-app/src/index.tsx
+++ datadog-app/src/index.tsx
@@ -8,14 +8,6 @@ const init = async () => {
             let widget = await import('./widget/GeoMap');
             return widget.default();
         }
-        case '/panel': {
-            let sidepanel = await import('./side-panel');
-            return sidepanel.default();
-        }
-        case '/modal': {
-            let modal = await import('./modal');
-            return modal.default();
-        }
         default: {
             let controller = await import('./controller');
             return controller.default();
