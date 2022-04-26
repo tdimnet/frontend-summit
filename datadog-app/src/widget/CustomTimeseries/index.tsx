dc976f0dfe4fb4fa248c4fc9fedc0b5633707e38
tdimnet <thomas.dimnet@datadoghq.com>
2022-04-26 14:15:18 +0200

    Query fron SDK

--- datadog-app/src/widget/CustomTimeseries/index.tsx
+++ datadog-app/src/widget/CustomTimeseries/index.tsx
@@ -1,10 +1,23 @@
 import { init } from '@datadog/ui-extensions-sdk'
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
 const client = init()
 
 function Widget() {
+    useEffect(() => {
+        client.api.get('/api/v1/query', {
+            params: {
+                from: '1647527858',
+                to: '1647532858',
+                query: 'system.cpu.idle{*}'
+            }
+        })
+        .then(data => console.log("=====", data))
+        .catch(err => console.log("An error occurs", err))
+    }, [])
+
+
     return (
         <h1>Ok, let's go</h1>
     )
